3594c426f8b2dbcba7021bac159cfa556992bead
cyrilledubois <sirdubois@gmail.com>
2017-11-09 10:07:18 +0100

    rajout page 2

---
+++ page2.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+    
+    <title>Page 2</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">    
+</head>
+<body>
+ 
+<h1>Je suis le roi du pétrole</h1>
+<p>C'est avec beaucoup de fric que je creuse des trous</p>
+<div class="container">
+	<div class="row">
+		<img src="Kong_HP-sn.jpg" alt="image singe">
+		<?php echo 'nous sommes le ' . date('d-m-Y') ; ?>
+	</div>
+</div>
+
+
+</body>
+</html>
