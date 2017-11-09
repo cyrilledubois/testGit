410b2fe823510f7f8ccc0a904ee7f8a2da5a8bf3
cyrilledubois <sirdubois@gmail.com>
2017-11-09 11:00:40 +0100

    Création page , modif page 2

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <html>
 <head>
     
-    <title>Page 2</title>
+    <title>Ma vie, mon oeuvre !</title>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">    
 </head>
 <body>
@@ -16,7 +16,6 @@
 		<?php echo 'nous sommes le ' . date('d-m-Y') ; ?>
 	</div>
 </div>
-<p>ET c'est comme cela que le pétrole sort de terre....</p>
 
 
 </body>
